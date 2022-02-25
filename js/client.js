59c9142e55b72bb135aedb4233cdd022d5ba59b5
abhishekmaliyal <abhishekmaliyal123@gmail.com>
2022-02-25 22:44:16 +0530

    second commit - added discriptions to code

--- js/client.js
+++ js/client.js
@@ -1,47 +1,52 @@
-const socket = io('http://localhost:8000');
+const socket = io("http://localhost:8000");
 
-// Get DOM elements in respective Js variables
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp')
-const messageContainer = document.querySelector(".container")
 
-var audio = new Audio('ting.mp3');//this is used to store an audio in a variable to be used later
+//socket.on --> listens and collects data
+//socket.emit --> sends data
+//socket.broadcast.emit --> sends data to all other than creator
 
-// Function which will append event info to the contaner
-const append = (message, position)=>{
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-    messageContainer.append(messageElement);
-    if(position =='left'){ 
-        audio.play();
-    }
-}
-// Ask new user for his/her name and let the server know
-const name = prompt("Enter your name to join");//this creates a pop-up window to get the name from the user and then stores it in name variable
-socket.emit('new-user-joined', name);
 
-// If a new user joins, receive his/her name from the server
-socket.on('user-joined', name =>{
-    append(`${name} just landed`, 'right')
-})
+//these are to get DOM elements in respective variables
+const form = document.getElementById("send_container");
+const messageInput = document.getElementById("messageInput");
+const messageContainer = document.querySelector(".container");
 
-// If server sends a message, receive it
-socket.on('receive', data =>{
-    append(`${data.name}: ${data.message}`, 'left')
-})
+var audio = new Audio("alert.mp3"); //this is used to store an audio in a variable to be used later
 
-// If a user leaves the chat, append the info to the container
-socket.on('left', name =>{
-    append(`${name} couldn't survive`, 'right')
-})
+//this function is used to append event information to the contaner
+const append = (message, position) => {
+  const messageElement = document.createElement("div"); //a div is created dynamically in HTML
+  messageElement.innerText = message; //message is put in the new div as text
+  messageElement.classList.add("message"); //message class is added to div
+  messageElement.classList.add(position); //position class is added to div for left and right messages
+  messageContainer.append(messageElement);
+  if (position == "left") {//this plays the audio when a message is recieved
+    audio.play();
+  }
+};
+const name = prompt("Enter your name to join"); //this creates a pop-up window to get the name from the user and then stores it in name variable
+socket.emit("new_user", name); //this function calls the new-user-joined function in the server to store the names of the users that joined
 
-// If the form gets submitted, send server the message
-form.addEventListener('submit', (e) => {
-    e.preventDefault();         //this is used to stop the page from refreshing after every submition
-    const message = messageInput.value;     //variable message stores the text entered in the text box
-    append(`You: ${message}`, 'right');     // this appends the the text in the message variable after "you:"
-    socket.emit('send', message);
-    messageInput.value = ''     //this is used to clear the message text box for fresh new message
-})
+socket.on("user-joined", (name) => {
+  //this function is for new users
+  append(`${name} just landed`, "right"); //this function will print that a new user has joined the chat
+});
+
+socket.on("receive", (data) => {
+  //this function gets the message sent by the user along with the name of the user that sent it
+  append(`${data.name}: ${data.message}`, "left"); //this sends the name and the message to local append function
+});
+
+socket.on("left", (name) => {
+  //this function gets he name of the user that has left the chat
+  append(`${name} couldn't survive`, "right"); //this displays the name of he user that left to the other remaining users
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //this is used to stop the page from refreshing after every submition
+  const message = messageInput.value; //variable message stores the text entered in the text box
+  append(`You: ${message}`, "right"); // this appends the the text in the message variable after "you:"
+  socket.emit("send", message);
+  messageInput.value = ""; //this is used to clear the message text box for fresh new message
+  scroll();
+});
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,24 +1,24 @@
 // Node server which will handle socket io connections
 const io = require('socket.io')(8000)
 
-const users = {};
+const users = {};//an object called user is created to store information of every user separately 
 
 io.on('connection', socket =>{
-    // If any new user joins, let other users connected to the server know!
-    socket.on('new-user-joined', name =>{ 
-        console.log("new user",name)
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+    //this function assigns the new user with a unique socket ID and tells other users that specified user has joined
+    socket.on('new_user', name =>{ 
+        console.log("new user",name)//this is for console/terminal
+        users[socket.id] = name;//as the new connection is established a unique ID is generated and the user is associated with it 
+        socket.broadcast.emit('user-joined', name);//this sends the name of the user that joined back to the client 
     });
 
-    // If someone sends a message, broadcast it to other people
+    //this function sends message to all the other user
     socket.on('send', message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})//when someone sends message this broadcasts it to others
     });
 
-    // If someone leaves the chat, let others know 
+    //this function gives the name of the user that left the chat
     socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
+        socket.broadcast.emit('left', users[socket.id]);//this tells other users that specified user left the chat
+        delete users[socket.id];//this deletes the user using the unique socket ID
     });
-})
+});
